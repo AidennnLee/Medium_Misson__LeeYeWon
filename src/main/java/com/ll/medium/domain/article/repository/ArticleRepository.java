ad9bd38d320e4a7355dc1a9ffb044a4db2a5112a
Aidennn-Lee <yw010804@gmail.com>
2023-12-14 18:27:14 +0900

    root page

--- src/main/java/com/ll/medium/domain/article/repository/ArticleRepository.java
+++ src/main/java/com/ll/medium/domain/article/repository/ArticleRepository.java
@@ -3,8 +3,12 @@ package com.ll.medium.domain.article.repository;
 import com.ll.medium.domain.article.entity.Article;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface ArticleRepository extends JpaRepository<Article, Integer> {
     Page<Article> findAll(Pageable pageable);
+    List<Article> findAll(Sort sort);
 }
